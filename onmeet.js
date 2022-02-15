22affa12d01033514c944055307796c61ed54ee7
SonYoungDeuk1 <deukzzang@gmail.com>
2022-02-15 16:20:53 +0900

    Update onmeet.js

--- onmeet.js
+++ onmeet.js
@@ -98,8 +98,9 @@ var videoFilterMode = null;
 var videoFilterLevel = -1;
 
 $(document).ready(function() {
-    $(window).resize();
-
+    $(window).resize(doResize);
+    doResize();
+    
     // 토큰이 없으면 로그인 페이지, 있으면 대시보드
     var check = getCookie()
     $('#user_logout').click(userLogout);
@@ -116,11 +117,12 @@ $(document).ready(function() {
 
 function doResize() {
     /* 윈도우창 크기 조절시 */ 
-    console.log("test");
+    console.log("doResize");
 
     resizeMultiVideo();
     resizeChatArea();
     resizeMemberListArea();
+    
 }
 
 function resizeMultiVideo() {
@@ -611,7 +613,7 @@ function auth() {
             if(json["result"] === "failure" || json["result"] === "error") {
                 var reason = json["data"]["reason"];
                 var errorCode = json["data"]["error_code"];
-                if(errorCode === 4101) {             
+                if(errorCode === 4101) {
                     alertMessage = "로그인 정보가 잘못되었습니다. ID 혹은 Password를 다시 확인해주세요.";
                 } else if(errorCode === 4102) {
                     alertMessage = "승인되지 않은 계정입니다. 관리자에게 문의해주세요.";
@@ -649,6 +651,7 @@ function auth() {
             }
         }
     });
+    
 }
 
 
@@ -3338,6 +3341,7 @@ function localFeedForVideoCall(isCaller) {
                         $('#videos').hide();
                         $('#curbitrate').hide();
                         $('#curres').hide();
+                        $('roomcontainer').hide();
                         ringingSound.load();
                         afterVideoCallEnd();
 
@@ -3617,6 +3621,7 @@ function localFeedForVideoCall(isCaller) {
                 $('#videos').hide();
                 $('#curbitrate').hide();
                 $('#curres').hide();
+                $('roomcontainer').hide();
                 if(bitrateTimer[1])
                     clearInterval(bitrateTimer[1]);
                 bitrateTimer[1] = null;
@@ -4446,6 +4451,7 @@ function localFeedForScreenSharing() {
             console.debug(" ::: Got a local stream :::", stream);
             $('#screenmenu').hide();
             $('#room').removeClass('hide').show();
+            $('roomcontainer').hide();
             resizeMultiVideo();
             if($('#screenvideo').length === 0) {
                 $('#screencapture').append('<video class="rounded centered" id="screenvideo" width="100%" height="100%" autoplay playsinline muted="muted"/>');
@@ -5434,16 +5440,16 @@ function addButtonMenu() {
                     '<div class="dropbtn ctrl-btn" id="more" style="cursor: pointer;">' +
                         '<img src="../icon/more.png" width="25" height="25" alt="" >' +
                         '<div class="dropup-content">' +
-                            '<button class="menu-btn" id="full_screen_btn">전체화면</button>' +
-                            '<button class="menu-btn" id="cfgBtn">설정</button>' +
+                            //'<button class="menu-btn" id="full_screen_btn">전체화면</button>' +
+                            //'<button class="menu-btn" id="cfgBtn">설정</button>' +
                         '</div>' +
                     '</div>' +
                 '</div>' +
-                '<div class="ctrl-btn" id="exit_btn" style="cursor: pointer; background: rgb(195,5,5);">' +
+                '<div class="ctrl-btn" id="exit_btn" style="cursor: pointer; <!--background: rgb(195,5,5);-->">' +
                     '<img src="../icon/log-out.png" width="25" height="25" alt="" >' +
                 '</div>' +
             '</div>' +
-            '<div class="side-ctrl-menu" id="side_ctrl_menu">' +
+            //'<div class="side-ctrl-menu" id="side_ctrl_menu">' +
             '</div>');
     } else {
         if(roomOwner) {
@@ -6166,7 +6172,7 @@ function testRecord() {
         stopRecording();
     }
 }
-
+/*
 function setupChatting() {
     console.log("setupChatting");
     var data = { request: "setup" };
@@ -6186,7 +6192,7 @@ function setupChatting() {
         }
     });
 }
-
+*/
 
 function exit() {
     console.log("exit");
@@ -6204,6 +6210,29 @@ function initSelectMenu() {
     $("#bitrate").val("1024").prop("selected", true);
 }
 
+function addLocalFeedLayout() {
+    console.log("addLocalFeedLayout");
+    $('#members').append(
+        '<div class="col-xs-3 user-video" id=local-video>' +
+            '<div class="video-bg" id="videolocal">' +
+                '<span class="user-name" id="myname"></span>' +
+            '</div>' +
+        '</div>');
+    resizeMultiVideo();
+}
+
+function addRemoteFeedLayout(remoteFeed) {
+    console.log("addRemoteFeedLayout");
+    $('#members').append(
+        '<div class="col-xs-3 user-video" id="r' + remoteFeed.rfindex + '">' +
+            '<div class="video-bg" id="videoremote' + remoteFeed.rfindex + '">' +
+                '<span class="user-name">' + remoteFeed.rfdisplay + '</span>' +
+            '</div>' +
+        '</div>');
+    resizeMultiVideo();
+}
+
+
 /* 발신자 레이아웃 */
 function addCallerLayout() {
     console.log("addCallerLayout");
@@ -6220,7 +6249,6 @@ function addCallerLayout() {
         '</div>');
     resizeMultiVideo();
 }
-
 /* 수신자 레이아웃 */
 function addCalleeLayout() {
     console.log("addCalleeLayout");
@@ -6228,32 +6256,6 @@ function addCalleeLayout() {
         '<div class="position-absolute top-0 end-0 callee-video" id=callee_video>' +
             '<div class="callee-video-bg" id="videoremote">' +
             '<span><p> hihihih123112312132132ihihihihi </p></span>' +
-                '<span class="user-name">' + calleeName + '</span>' +
-            '</div>' +
-        '</div>');
-    resizeMultiVideo();
-}
-
-function addCallerLayout() {
-    console.log("addCallerLayout");
-    $('#videos').append(
-        '<div class="video-call" id=video_call>' +
-        '</div>'
-    );
-    $('#video_call').append(
-        '<div class="col-xs-3 caller-video" id=caller_video>' +
-            '<div class="caller-video-bg" id="videolocal">' +
-                '<span class="user-name" id="myname"></span>' +
-            '</div>' +
-        '</div>');
-    resizeMultiVideo();
-}
-
-function addCalleeLayout() {
-    console.log("addCalleeLayout");
-    $('#video_call').append(
-        '<div class="col-xs-3 callee-video" id=callee_video>' +
-            '<div class="callee-video-bg" id="videoremote">' +
                 '<span class="user-name">' + calleeName + '</span>' +
             '</div>' +
         '</div>');
@@ -6310,6 +6312,9 @@ function afterVideoCallStart() {
     $('#members').hide();
     $('#dashboard').hide();
     $('#logo').hide();
+    /* 화상통화 연결 후 설정/회의방/채팅방 hide*/
+    $('#roomcontainer').hide();
+    $("#configModal").hide();
 }
 
 // VideoCall, VideoConf 종료 후 처리
@@ -6330,6 +6335,9 @@ function afterVideoCallEnd() {
     $('#members').removeClass('hide').show();
     $('#dashboard').removeClass('hide').show();
     $('#logo').removeClass('hide').show();
+    /* 영상통화 관련 로직 처리 후 종료됐을 때 roomcontainer, configModal show*/
+    $('#roomcontainer').removeClass('hide').show();
+    $("#configModal").removeClass('hide').show();
     closeNav();
     closeMembers();
     $('#joined_member *').remove();
